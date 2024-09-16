a6375f4667bbe2880536699ed084a7c30e928314
jumathhh <mathju2024@gmail.com>
2024-09-16 09:05:22 -0300

    seg-16

---
+++ script.js
@@ -0,0 +1,12 @@
+
+//alert("Testando o script");
+//caixa de texto no navegador
+
+var nome = prompt("Qual seu nome? ");
+//caixa de ação para insserir nome
+
+var sobrenome = prompt("Qual seu sobrenome? ");
+
+console.log("Olá "+nome+" "+sobrenome);
+
+//document.write("Olá "+nome+" "+sobrenome);
